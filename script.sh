2508280d0d013611c251694bc94e4ee70ae85a8f
Aleksei Krasnoperov <aleksei@krasnoperov.me>
2019-03-25 19:24:11 +0800

    Disable notifications because Builderr handles Slack notifications by itself

--- script.sh
+++ script.sh
@@ -1,25 +1,3 @@
 #!/bin/sh
 
-SLACK_COLOR=''
-
-if [ ! -z "${SECRET_SLACK_WEBHOOK_URL}" ]
-then
-  if [ ! -z "${PLUGIN_START}" ]
-  then
-    SLACK_MESSAGE="\`<${DRONE_BUILD_LINK}|${DRONE_BUILD_NUMBER}>\` build of <${DRONE_COMMIT_LINK}|${DRONE_REPO_NAME}:${DRONE_BRANCH}> started"
-  else
-
-    if [ "${DRONE_BUILD_STATUS}" = "success" ]
-    then
-      SLACK_MESSAGE="\`<${DRONE_BUILD_LINK}|${DRONE_BUILD_NUMBER}>\` build of <${DRONE_COMMIT_LINK}|${DRONE_REPO_NAME}:${DRONE_BRANCH}> completed"
-      SLACK_COLOR='good'
-    else
-      SLACK_MESSAGE="\`<${DRONE_BUILD_LINK}|${DRONE_BUILD_NUMBER}>\` build of <${DRONE_COMMIT_LINK}|${DRONE_REPO_NAME}:${DRONE_BRANCH}> failed"
-      SLACK_COLOR='danger'
-    fi
-
-  fi
-  curl -X POST --data-urlencode "payload={ 'username': 'CI', 'icon_emoji': ':drone:', 'attachments': [ { 'color': '${SLACK_COLOR}', 'text': '${SLACK_MESSAGE}', 'mrkdwn_in': ['text'] } ] }" ${SECRET_SLACK_WEBHOOK_URL}
-else
-  echo "There is no SECRET_SLACK_WEBHOOK_URL environment variable"
-fi
+# Do nothing because Builderr already handles Slack notifications
